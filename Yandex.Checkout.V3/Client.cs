e53af73f87ac48c0a7f4fd21fdc05d165a4cf298
Ilya Chudin <chudinia@gmail.com>
2018-10-09 17:26:38 +0500

    async extensions
    net 452

--- Yandex.Checkout.V3/Client.cs
+++ Yandex.Checkout.V3/Client.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Yandex.Checkout.V3
 {
@@ -68,6 +69,52 @@ namespace Yandex.Checkout.V3
             return Post<Payment>(payment, _apiUrl + payment.id, idempotenceKey);
         }
 
+        #region Async
+#if !NET40
+        /// <summary>
+        /// Payment creation
+        /// </summary>
+        /// <param name="payment">Payment information, <see cref="NewPayment"/></param>
+        /// <param name="idempotenceKey">Idempotence key, use <value>null</value> to generate new one</param>
+        /// <returns><see cref="Payment"/></returns>
+        public Task<Payment> CreatePaymentAsync(NewPayment payment, string idempotenceKey = null)
+            => PostAsync<Payment>(payment, _apiUrl, idempotenceKey);
+
+        /// <summary>
+        /// Payment capture
+        /// </summary>
+        /// <param name="payment">Payment information, <see cref="Payment"/></param>
+        /// <param name="idempotenceKey">Idempotence key, use <value>null</value> to generate new one</param>
+        /// <returns><see cref="Payment"/></returns>
+        public Task<Payment> CaptureAsync(Payment payment, string idempotenceKey = null)
+            => PostAsync<Payment>(payment, _apiUrl + payment.id + "/capture", idempotenceKey);
+
+        /// <summary>
+        /// Query payment state
+        /// </summary>
+        /// <param name="payment">Payment information, <see cref="Payment"/></param>
+        /// <param name="idempotenceKey">Idempotence key, use <value>null</value> to generate new one</param>
+        /// <returns><see cref="Payment"/></returns>
+        public Task<Payment> QueryPaymentAsync(Payment payment, string idempotenceKey = null)
+            => PostAsync<Payment>(payment, _apiUrl + payment.id, idempotenceKey);
+
+        private async Task<T> PostAsync<T>(object body, string url, string idempotenceKey)
+        {
+            var request = CreateRequest<T>(body, url, idempotenceKey);
+
+            using (var response = await request.GetResponseAsync())
+            using (var responseStream = response.GetResponseStream())
+            using (var sr = new StreamReader(responseStream ?? throw new InvalidOperationException("Response stream is null.")))
+            {
+                string jsonResponse = await sr.ReadToEndAsync();
+                T info = JsonConvert.DeserializeObject<T>(jsonResponse);
+                return info;
+            }
+        }
+#endif
+        #endregion Async
+
+
         /// <summary>
         /// Parses an HTTP request into a <see cref="Message"/> object.
         /// </summary>
@@ -103,26 +150,7 @@ namespace Yandex.Checkout.V3
 
         private T Post<T>(object body, string url, string idempotenceKey)
         {
-            WebRequest request = WebRequest.Create(url);
-            request.Method = "POST";
-            request.ContentType = "application/json";
-            request.Headers.Add("Authorization", _authorization);
-
-            // Похоже, что этот заголовок обязателен, без него говорит (400) Bad Request.
-            request.Headers.Add("Idempotence-Key", idempotenceKey ?? Guid.NewGuid().ToString());
-
-            if (_userAgent != null)
-            {
-                ((HttpWebRequest)request).UserAgent = _userAgent;
-            }
-
-            string json = JsonConvert.SerializeObject(body);
-            byte[] postBytes = Encoding.UTF8.GetBytes(json);
-            request.ContentLength = postBytes.Length;
-            using (Stream stream = request.GetRequestStream())
-            {
-                stream.Write(postBytes, 0, postBytes.Length);
-            }
+            var request = CreateRequest<T>(body, url, idempotenceKey);
 
             using (WebResponse response = request.GetResponse())
             using (var responseStream = response.GetResponseStream())
@@ -132,6 +160,32 @@ namespace Yandex.Checkout.V3
                 T info = JsonConvert.DeserializeObject<T>(jsonResponse);
                 return info;
             }
+        }
+
+        private WebRequest CreateRequest<T>(object body, string url, string idempotenceKey)
+        {
+            var request = WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", _authorization);
+
+            // Похоже, что этот заголовок обязателен, без него говорит (400) Bad Request.
+            request.Headers.Add("Idempotence-Key", idempotenceKey ?? Guid.NewGuid().ToString());
+
+            if (_userAgent != null)
+            {
+                ((HttpWebRequest) request).UserAgent = _userAgent;
+            }
+
+            var json = JsonConvert.SerializeObject(body);
+            var postBytes = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = postBytes.Length;
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(postBytes, 0, postBytes.Length);
+            }
+
+            return request;
         }
     }
 }
