71316f452757a0a917c95f2508fa04f5b96ff081
Björn Sandman <33433576+bsandman80@users.noreply.github.com>
2020-08-17 17:56:22 +0200

    Added issuer country and name to card payment method (#16)

--- Yandex.Checkout.V3.Tests/SerializerTests.cs
+++ Yandex.Checkout.V3.Tests/SerializerTests.cs
@@ -32,5 +32,25 @@ namespace Yandex.Checkout.V3.Tests
             var s = Serializer.SerializeObject(new Refund {});
             Assert.AreEqual("{\"status\":\"pending\",\"created_at\":\"0001-01-01T00:00:00\"}", s);
         }
+
+        [TestMethod]
+        public void CardPaymentMethodDeserializedCorrectly()
+        {
+            var card = Serializer.DeserializeObject<Card>("{" +
+                                                          "\"first6\": \"555555\"," +
+                                                          "\"last4\": \"4444\"," +
+                                                          "\"expiry_month\": \"07\"," +
+                                                          "\"expiry_year\": \"2022\"," +
+                                                          "\"card_type\": \"MasterCard\"," +
+                                                          "\"issuer_country\": \"RU\"," +
+                                                          "\"issuer_name\": \"Sberbank\"}");
+            Assert.AreEqual("555555", card.First6);
+            Assert.AreEqual("4444", card.Last4);
+            Assert.AreEqual("07", card.ExpiryMonth);
+            Assert.AreEqual("2022", card.ExpiryYear);
+            Assert.AreEqual("MasterCard", card.CardType);
+            Assert.AreEqual("RU", card.IssuerCountry);
+            Assert.AreEqual("Sberbank", card.IssuerName);
+        }
     }
 }
