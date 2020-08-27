719fbf1894d6446adaa1c02b0bd71292ed2edd36
alexanderkolbyoshkin <42632260+alexanderkolbyoshkin@users.noreply.github.com>
2020-08-27 09:22:35 +0300

    Added AccountId to Recipient (#19)
    Co-authored-by: Kolbeshkin Aleksandr <aleksandr.kolbeshkin@renhealth.com>

--- Yandex.Checkout.V3/Recipient.cs
+++ Yandex.Checkout.V3/Recipient.cs
@@ -2,6 +2,11 @@
 {
     public class Recipient
     {
+        /// <summary>
+        /// Идентификатор магазина в Яндекс.Кассе
+        /// </summary>
+        public string AccountId { get; set; }
+
         public string GatewayId { get; set; }
     }
 }
