76ae0c42cc99ffdb5eb61427d2220d3b9933b27a
Sergey Slepov <slepov@gmail.com>
2021-06-02 11:39:04 +0100

    Лишняя запятая в комментарии «Чек, для проведения возврата»

--- Yandex.Checkout.V3/NewRefund.cs
+++ Yandex.Checkout.V3/NewRefund.cs
@@ -20,7 +20,7 @@ namespace Yandex.Checkout.V3
         public string PaymentId { get; set; }
 
         /// <summary>
-        /// Чек, для проведения возврата по 54-ФЗ <see cref="V3.Receipt"/>
+        /// Чек для проведения возврата по 54-ФЗ <see cref="V3.Receipt"/>
         /// </summary>
         public Receipt Receipt { get; set; }
 
