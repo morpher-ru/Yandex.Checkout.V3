319ebdc0266a212150e7d7a089aaeaa96a8b505e
YurasovDV <yurasovdv@gmail.com>
2021-06-02 15:47:34 +0300

    Добавил в чек информацию о поставщике (#36)

---
+++ Yandex.Checkout.V3/Supplier.cs
@@ -0,0 +1,23 @@
+﻿namespace Yandex.Checkout.V3
+{
+    /// <summary>
+    /// Информация о поставщике товара или услуги
+    /// </summary>
+    public class Supplier
+    {
+        /// <summary>
+        /// Наименование поставщика
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Телефон поставщика. Указывается в формате ITU-T E.164
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// ИНН поставщика (10 или 12 цифр)
+        /// </summary>
+        public string Inn { get; set; }
+    }
+}
