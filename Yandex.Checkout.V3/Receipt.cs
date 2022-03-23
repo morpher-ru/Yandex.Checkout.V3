f9aef70946e632fca90bc452178ff4960bcb9885
Oleg <sagello@rambler.ru>
2022-03-23 14:41:11 +0300

    Add Receipt.Customer (#40)
    receipt.phone и receipt.email устарели
    надо передавать через customer
    документация https://yookassa.ru/developers/api?lang=bash#create_payment

--- Yandex.Checkout.V3/Receipt.cs
+++ Yandex.Checkout.V3/Receipt.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Yandex.Checkout.V3
 {
@@ -8,13 +9,22 @@ namespace Yandex.Checkout.V3
     public class Receipt
     {
         /// <summary>
-        /// Номер телефона плательщика
+        /// Информация о пользователе. Необходимо указать как минимум контактные данные:
+        /// электронную почту (customer.email <see cref="V3.Customer.Email"/>) или 
+        /// номер телефона (customer.phone <see cref="V3.Customer.Phone"/>).
         /// </summary>
+        public Customer Customer { get; set; }
+
+        /// <summary>
+        /// Телефон пользователя для отправки чека. Указывается в формате ITU-T E.164, например 79000000000. 
+        /// </summary>
+        [Obsolete("Данные рекомендуется передавать в параметре receipt.customer.phone.")]
         public string Phone { get; set; }
 
         /// <summary>
-        /// Электронная почта плательщика
+        /// Электронная почта пользователя для отправки чека.
         /// </summary>
+        [Obsolete("Данные рекомендуется передавать в параметре")]
         public string Email { get; set; }
 
         /// <summary>
