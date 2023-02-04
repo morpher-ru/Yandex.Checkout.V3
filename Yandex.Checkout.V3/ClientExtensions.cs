1655117a90bee1f9c521bf2c46146362645f8af2
Igor Postanogov <ipostanogov@outlook.com>
2023-02-04 08:56:59 +0500

    refactor: Backward compatibility, explicit disposal

--- Yandex.Checkout.V3/ClientExtensions.cs
+++ Yandex.Checkout.V3/ClientExtensions.cs
@@ -5,8 +5,19 @@ namespace Yandex.Checkout.V3
 {
     public static class ClientExtensions
     {
-        public static AsyncClient MakeAsync(this Client client, TimeSpan? timeout = null, HttpClient httpClient = null)
+        public static AsyncClient MakeAsync(this Client client) =>
+            MakeAsync(client, null, null, true);
+
+        public static AsyncClient MakeAsync(this Client client, TimeSpan timeout) =>
+            MakeAsync(client, timeout, null, true);
+
+        public static AsyncClient MakeAsync(this Client client, TimeSpan? timeout,
+            HttpClient httpClient, bool disposeOfHttpClient = false)
         {
+            if (httpClient == null)
+            {
+                disposeOfHttpClient = true;
+            }
             httpClient ??= new HttpClient();
             httpClient.BaseAddress = new Uri(client.ApiUrl);
             httpClient.DefaultRequestHeaders.Add("Authorization", client.Authorization);
@@ -14,7 +25,7 @@ namespace Yandex.Checkout.V3
             {
                 httpClient.Timeout = timeout.Value;
             }
-            return new AsyncClient(httpClient, true);
+            return new AsyncClient(httpClient, disposeOfHttpClient);
         }
     }
 }
