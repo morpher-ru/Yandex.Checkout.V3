16ac64f02c6a169f2af7194e20623fee66d40eed
Sergey Slepov <slepov@gmail.com>
2024-01-23 18:58:35 +0000

    Add XML comments for Rrn and AuthCode

--- Yandex.Checkout.V3/AuthorizationDetails.cs
+++ Yandex.Checkout.V3/AuthorizationDetails.cs
@@ -3,7 +3,15 @@
     // ReSharper disable once ClassNeverInstantiated.Global
     public class AuthorizationDetails
     {
+        /// <summary>
+        /// Retrieval Reference Number, a unique identifier of a transaction in the issuer's system.
+        /// Used for payments via bank card.
+        /// </summary>
         public string Rrn { get; set; }
+        
+        /// <summary>
+        /// The authorization code provided by the bank card issuer to confirm authorization.
+        /// </summary>
         public string AuthCode { get; set; }
     }
 }
