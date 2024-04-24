1ad39d5723c8a39cd8f27936b3627f3858662a8f
Alexander <ostetic@gmail.com>
2024-04-24 22:11:44 +0400

    Create RefundList.cs

---
+++ Yandex.Checkout.V3/RefundList.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+
+namespace Yandex.Checkout.V3
+{
+    public class RefundList
+    {
+        [JsonProperty("type")]
+        public string ResponseKind { get; set; }
+
+        [JsonProperty("items")]
+        public List<Refund> Refunds { get; set; }
+
+        [JsonProperty("next_cursor")]
+        public string NextCursor { get; set; }
+    }
+}
