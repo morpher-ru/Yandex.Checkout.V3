fe1cd595f31e965bda1d7cd3f397ac9eb54eb4e9
Sergey Slepov <slepov@gmail.com>
2024-04-25 10:26:01 +0800

    Add some more GetReceiptsFilterExtensionsTests

--- Yandex.Checkout.V3.Tests/GetReceiptsFilterExtensionsTests.cs
+++ Yandex.Checkout.V3.Tests/GetReceiptsFilterExtensionsTests.cs
@@ -7,6 +7,16 @@ namespace Yandex.Checkout.V3.Tests
     public class GetReceiptsFilterExtensionsTests
     {
         [TestMethod]
+        public void CreateRequestUrl_EmptyFilter_EmptyQuery()
+        {
+            var filter = new GetReceiptsFilter();
+
+            string url = filter.CreateRequestUrl(null);
+
+            Assert.AreEqual("receipts", url);
+        }
+        
+        [TestMethod]
         public void CreateRequestUrlDateTest()
         {
             var filter = new GetReceiptsFilter
@@ -17,6 +27,36 @@ namespace Yandex.Checkout.V3.Tests
             string url = filter.CreateRequestUrl(null);
 
             string expected = "receipts?created_at.lte=2024-04-24T15:30:00Z";
+            
+            Assert.AreEqual(expected, url);
+        }
+        
+        [TestMethod]
+        public void CreateRequestUrl_StatusPending()
+        {
+            var filter = new GetReceiptsFilter
+            {
+                Status = ReceiptStatus.Pending
+            };
+
+            string url = filter.CreateRequestUrl(null);
+
+            string expected = "receipts?status=pending";
+            
+            Assert.AreEqual(expected, url);
+        }
+        
+        [TestMethod]
+        public void CreateRequestUrl_Limit42()
+        {
+            var filter = new GetReceiptsFilter
+            {
+                Limit = 42
+            };
+
+            string url = filter.CreateRequestUrl(null);
+
+            string expected = "receipts?limit=42";
             
             Assert.AreEqual(expected, url);
         }
