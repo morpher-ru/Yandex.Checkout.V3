be4fd65c3bfcb04bd035a4ed1fa5ad72d715e956
Sergey Slepov <slepov@gmail.com>
2024-04-28 20:19:17 +0800

    Make Program static

--- Yandex.Checkout.V3.Demo/Program.cs
+++ Yandex.Checkout.V3.Demo/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Yandex.Checkout.V3.Demo
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
