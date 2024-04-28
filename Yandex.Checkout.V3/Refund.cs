72bbb9aafdb695000b2ac510a86a744f843d13d4
Sergey Slepov <slepov@gmail.com>
2024-04-28 20:49:14 +0800

    Remove unused usings

--- Yandex.Checkout.V3/Refund.cs
+++ Yandex.Checkout.V3/Refund.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Yandex.Checkout.V3
 {
--- Yandex.Checkout.V3/Serializer.cs
+++ Yandex.Checkout.V3/Serializer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace Yandex.Checkout.V3
