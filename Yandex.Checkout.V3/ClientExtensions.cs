50d23a4a8dd4f01fc35979dc6b1135bb74d61f75
Sergey Slepov <slepov@gmail.com>
2024-06-22 12:25:40 +0100

    Allow use of an existing HttpClient in MakeAsync()

--- Yandex.Checkout.V3/ClientExtensions.cs
+++ Yandex.Checkout.V3/ClientExtensions.cs
@@ -14,15 +14,26 @@ public static class ClientExtensions
         return new AsyncClient(httpClient, true);
     }
 
+    public static AsyncClient MakeAsync(this Client client, HttpClient httpClient)
+    {
+        SetHttpClientProperties(client, httpClient);
+        return new AsyncClient(httpClient, disposeOfHttpClient: false);
+    }
+
     private static HttpClient NewHttpClient(Client client)
     {
-        var httpClient = new HttpClient {BaseAddress = new Uri(client.ApiUrl)};
-        
+        var httpClient = new HttpClient();
+        SetHttpClientProperties(client, httpClient);
+        return httpClient;
+    }
+
+    private static void SetHttpClientProperties(Client client, HttpClient httpClient)
+    {
+        httpClient.BaseAddress = new Uri(client.ApiUrl);
+
         httpClient.DefaultRequestHeaders.Add("Authorization", client.Authorization);
 
         if (!string.IsNullOrEmpty(client.UserAgent))
             httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(client.UserAgent);
-        
-        return httpClient;
     }
 }
