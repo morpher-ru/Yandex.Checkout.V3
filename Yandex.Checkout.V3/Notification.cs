00160c3e45d3e081c63de9fe54fa36dfdf35ccff
bzaar <slepov@gmail.com>
2024-07-10 15:40:35 +0100

    Add payout notifications (#72)

--- Yandex.Checkout.V3/Notification.cs
+++ Yandex.Checkout.V3/Notification.cs
@@ -5,3 +5,5 @@ public record PaymentWaitingForCaptureNotification(Payment Object) : Notificatio
 public record PaymentSucceededNotification(Payment Object) : Notification;
 public record PaymentCanceledNotification(Payment Object) : Notification;
 public record RefundSucceededNotification(Refund Object) : Notification;
+public record PayoutSucceededNotification(Payout Object) : Notification;
+public record PayoutCanceledNotification(Payout Object) : Notification;
